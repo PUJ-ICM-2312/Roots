472158fe20122415e0c58d85a9d79ac638ff1739
Davidp1905 <adperezcely@gmail.com>
2025-04-29 21:40:56 -0500

    added more properties

--- Roots/app/src/main/java/com/example/roots/model/TipoPublicacion.kt
+++ Roots/app/src/main/java/com/example/roots/model/TipoPublicacion.kt
@@ -3,5 +3,6 @@ package com.example.roots.model
 enum class TipoPublicacion(val label: String) {
     Venta("Venta"),
     Arriendo("Arriendo"),
-    Temporada("Temporada")
+    Temporada("Temporada"),
+    Permuta("Permuta")
 }
